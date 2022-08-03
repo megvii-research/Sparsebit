9372444be766ad506bd5d5a80bf38bc2b766b793
zhber <32612451+zhber@users.noreply.github.com>
2022-08-03 11:08:46 +0800

    Refactor calibration (#15)

--- sparsebit/quantization/tools/__init__.py
+++ sparsebit/quantization/tools/__init__.py
@@ -1,3 +1,4 @@
 from .errors_profiler import QuantizationErrorProfiler, SharedData
 from .graph_wrapper import fx_symbolic_trace
+from .tensor_wrapper import to_detach, to_cpu
 from .calibration import *
--- sparsebit/quantization/tools/calibration.py
+++ sparsebit/quantization/tools/calibration.py
@@ -2,88 +2,106 @@ import torch
 from functools import partial
 
 from sparsebit.quantization.modules import QuantOpr
-from sparsebit.quantization.tools import SharedData
+from .graph_wrapper import GraphVisitor, fx_symbolic_trace
+from .tensor_wrapper import to_cpu, to_device, to_detach
 
 
-def get_topo(model):
-    calib_cache = SharedData()
-    for node in model.graph.nodes:
-        if node.op in ["output"]:  # skip output empty node
-            continue
-        node_name = node.name
+class CalibrationRunner(object):
+    def __init__(self, model):
+        self.model = fx_symbolic_trace(model)
 
-        # 输入module名称，None表示不是module，此时没有量化，可以直接使用float输入值作为量化输入值。
-        input_node_names = []
-        for input_node in node.args:
-            if not isinstance(input_node, torch.fx.node.Node) or input_node.op in [
-                "output",
-            ]:
-                input_node_names.append(None)
-            else:
-                input_node_names.append(input_node.name)
-        calib_cache.add_node(node_name, input_node_names)
-    return calib_cache
-
-
-def register_hook(model, calib_cache):
-    def _forward_hook(module, x_in, x_out, name):
-        prev_data = calib_cache.get_value(name)
-        if prev_data is None:
-            prev_data = []
-        assert isinstance(
-            prev_data, list
-        ), "calibration data must be cached in list format!"
-        prev_data.append(x_in[0].cpu())
-        calib_cache.set_value(name, prev_data)
-
-    calibration_handles = []
-    for node in model.graph.nodes:
-        if node.op == "placeholder":
-            for n in node.users:
-                module = getattr(model, n.target)
-                quant_node = n
-                while not (isinstance(module, QuantOpr) and module.input_quantizer):
-                    quant_node = n.next
-                    module = getattr(model, quant_node.target)
-
-                assert (
-                    len(module._forward_hooks) == 0
-                ), "only support one input in calibration now"
-
-                h = module.register_forward_hook(
-                    hook=partial(_forward_hook, name=quant_node.prev.name)
-                )
-                calibration_handles.append(h)
-    return calibration_handles
-
-
-def feature_layerwise_calibration(model, calib_cache, device):
-    for node in model.graph.nodes:
-        if (
-            node.op in ["placeholder", "output"]
-            or calib_cache.get_value(node.prev.name) is None
-        ):
-            continue
-        module = getattr(model, node.target)
-        if isinstance(module, QuantOpr) and module.input_quantizer:
-            for x in calib_cache.get_value(node.prev.name):
-                module.input_quantizer.update_observer(x)
-            module.input_quantizer.calc_qparams()
-            module.input_quantizer.observer.reset_data_cache()
-        inputs = zip(
-            *[
-                calib_cache.get_value(node.all_input_nodes[i].name)
-                for i in range(len(node.all_input_nodes))
-            ]
+    def prepare_calibration(self):
+        input_names_cache = set(
+            i.target for i in self.model.graph.nodes if i.op == "placeholder"
         )
-        with torch.no_grad():
-            outputs = []
-            for batch in inputs:
-                batch = [input.to(device) for input in batch]
-                outputs.append(module(*batch).cpu())
-        calib_cache.set_value(node.name, outputs)
-        calib_cache.finish_node(node.name)
 
-    assert len(calib_cache.outputs) == 0
-    assert len(calib_cache.output_degrees) == 0
-    del calib_cache
+        # tile outputs in input nodes
+        def _forward_hook(module, x_in, x_out, node, storage, record_names):
+            def flatten(x):
+                if isinstance(x, (list, tuple)):
+                    out = []
+                    for i in x:
+                        out.extend(flatten(i))
+                    return tuple(out)
+                return (x,)
+
+            pos = 0
+            flatten_x_in = flatten(x_in)
+            flatten_args = flatten(node.args)
+            for _x_in, _args in zip(flatten_x_in, flatten_args):
+                if isinstance(_args, torch.fx.Node) and _args.target in record_names:
+                    input_name = _args.target
+                    datas = storage.get_output(input_name)
+                    if datas is None:
+                        datas = []
+                    datas.append(to_cpu(to_detach(x_in[pos])))
+                    storage.set_output(input_name, datas)
+
+        def hook_wrapper(node, module, storage):
+            hooks = []
+            input_names = [
+                inp_node.target
+                for inp_node in node.all_input_nodes
+                if inp_node.target in input_names_cache
+            ]
+            if len(input_names) > 0:
+                hooks.append(
+                    module.register_forward_hook(
+                        hook=partial(
+                            _forward_hook,
+                            node=node,
+                            storage=storage,
+                            record_names=input_names,
+                        )
+                    )
+                )
+                for input_name in input_names:
+                    input_names_cache.remove(input_name)
+
+            return hooks
+
+        self.builder = GraphVisitor(self.model, hook_wrapper)
+
+    def feature_layerwise_calibration(self, device):
+        # manual forward once to calculate calibration
+        assert hasattr(self, "builder"), "run self.prepare_calibration first!"
+        batch_num = None
+        for node in self.model.graph.nodes:
+            if node.op in ["placeholder", "output"]:
+                if batch_num is None:
+                    batch_num = len(self.builder.storage.get_output(node.target))
+                continue
+
+            assert batch_num is not None
+
+            module = getattr(self.model, node.target)
+            if isinstance(module, QuantOpr) and getattr(
+                module, "input_quantizer", None
+            ):
+                for inp_node in node.all_input_nodes:
+                    inp_tensors = self.builder.storage.get_output(inp_node.target)
+                    for inp_tensor in inp_tensors:
+                        module.input_quantizer.update_observer(inp_tensor)
+                module.input_quantizer.calc_qparams()
+                module.input_quantizer.observer.reset_data_cache()
+
+            with torch.no_grad():
+                outputs = []
+                for batch_idx in range(batch_num):
+                    if node.op == "get_attr":  # is constant value
+                        outputs.append(to_cpu(module.data))
+                        continue
+                    args = self.builder.storage.extract_node_args(
+                        node.args, batch=batch_idx
+                    )
+                    args = to_device(args, device)
+                    # more time for less cuda memory occupation
+                    outputs.append(to_cpu(module(*args, **node.kwargs)))
+            self.builder.storage.set_output(node.target, outputs)
+            self.builder.storage.finish_node(node.target)
+
+    def weight_calibration(self):
+        for n, m in self.model.named_modules():
+            if isinstance(m, QuantOpr) and getattr(m, "weight_quantizer", None):
+                m.weight_quantizer.update_observer(m.weight)
+                m.weight_quantizer.calc_qparams()
