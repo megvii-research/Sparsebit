517bbc21bfa16c9c1bb86fc9566fc3e943e152af
李铮02-ISP二组 <lizheng02@megvii.com>
2022-08-09 15:34:08 +0800

    fix lsq

--- sparsebit/quantization/quantizers/lsq.py
+++ sparsebit/quantization/quantizers/lsq.py
@@ -35,7 +35,11 @@ class Quantizer(BaseQuantizer):
         self.observer.reset_data_cache()
         if not self.init_params:
             if self.is_perchannel:
-                scale = 2 * x_oc.abs().mean(axis=1) / math.sqrt(self.qdesc.qmax)
+                scale = (
+                    2
+                    * x_oc.reshape(x_oc.shape[0], -1).abs().mean(axis=1)
+                    / math.sqrt(self.qdesc.qmax)
+                )
             else:
                 scale = 2 * x_oc.abs().mean() / math.sqrt(self.qdesc.qmax)
             self.scale = nn.Parameter(self._broadcast_qparams(scale)).to(self.device)
