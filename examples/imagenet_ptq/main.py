3a4373c8e27b9b19e5fd36e7c00c332dfa51e4a8
sunpeiqin <speiqin@gmail.com>
2022-08-16 22:35:45 +0800

    init ptq demo

--- examples/imagenet_ptq/main.py
+++ examples/imagenet_ptq/main.py
@@ -22,7 +22,6 @@ import torchvision.models as models
 
 from sparsebit.quantization import QuantModel, parse_qconfig
 
-
 model_names = sorted(
     name
     for name in models.__dict__
@@ -147,32 +146,6 @@ parser.add_argument(
 
 def main():
     args = parser.parse_args()
-
-    # only support multi-cards in a machine
-    if args.multiprocessing_distributed:
-        RANK = int(os.environ["RANK"])
-        WORLD_SIZE = int(os.environ["WORLD_SIZE"])
-        LOCAL_RANK = int(os.environ["LOCAL_RANK"])
-        args.gpu = LOCAL_RANK
-        torch.cuda.set_device(LOCAL_RANK)
-        ngpus_per_node = torch.cuda.device_count()
-        args.batch_size = int(args.batch_size / ngpus_per_node)
-        args.workers = int((args.workers + ngpus_per_node - 1) / ngpus_per_node)
-        dist_url = "env://"
-        print("| distributed init (rank {}): {}".format(RANK, dist_url), flush=True)
-        torch.distributed.init_process_group(
-            backend="nccl", init_method=dist_url, world_size=WORLD_SIZE, rank=RANK
-        )
-        torch.distributed.barrier()
-        print(args)
-        setup_for_distributed(LOCAL_RANK == 0)
-    else:
-        if args.gpu is None:
-            if not torch.cuda.is_available():
-                raise "no cuda device exist, we assume you have at least one cuda device"
-            else:
-                args.gpu = 0  # default set gpu device 0
-
     if args.pretrained:
         print("=> using pre-trained model '{}'".format(args.arch))
         model = models.__dict__[args.arch](pretrained=True)
@@ -181,7 +154,12 @@ def main():
         model = models.__dict__[args.arch]()
 
     qconfig = parse_qconfig(args.qconfig)
-    model = QuantModel(model, config=qconfig).cuda()
+    qmodel = QuantModel(model, config=qconfig)
+
+    if torch.cuda.is_available():
+        qmodel.cuda()
+
+    cudnn.benchmark = True
 
     # Data loading code
     traindir = os.path.join(args.data, 'train')
@@ -191,8 +169,8 @@ def main():
     train_dataset = datasets.ImageFolder(
         traindir,
         transforms.Compose([
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
             transforms.ToTensor(),
             normalize,
         ]))
@@ -204,21 +182,13 @@ def main():
             transforms.ToTensor(),
             normalize,
         ]))
-    if args.multiprocessing_distributed:
-        train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
-        val_sampler = torch.utils.data.distributed.DistributedSampler(
-            val_dataset, shuffle=False, drop_last=True
-        )
-    else:
-        train_sampler = None
-        val_sampler = None
     train_loader = torch.utils.data.DataLoader(
         train_dataset,
         batch_size=args.batch_size,
-        shuffle=(train_sampler is None),
+        shuffle=True,
         num_workers=args.workers,
         pin_memory=True,
-        sampler=train_sampler,
+        sampler=None,
     )
     val_loader = torch.utils.data.DataLoader(
         val_dataset,
@@ -226,137 +196,26 @@ def main():
         shuffle=False,
         num_workers=args.workers,
         pin_memory=True,
-        sampler=val_sampler,
+        sampler=None,
     )
 
-    # set head and tail of model is 8bit
-    model.model.conv1.weight_quantizer.set_bit(bit=8)
-    model.model.fc.input_quantizer.set_bit(bit=8)
-    model.model.fc.weight_quantizer.set_bit(bit=8)
-    model.prepare_calibration()
-    calib_size, cur_size = 256, 0
-    for data, target in train_loader:
-        model(data.cuda())
-        cur_size += data.shape[0]
-        if cur_size >= calib_size:
-            break
-    model.init_QAT()
-    print(model.model)
-    if args.multiprocessing_distributed:
-        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+    # a calibration process
+    qmodel.prepare_calibration()
+    calibration_size, cur_size = 256, 0
+    model.eval()
+    with torch.no_grad():
+        for data, target in train_loader:
+            res = qmodel(data.cuda())
+            cur_size += data.shape[0]
+            if cur_size >= calibration_size:
+                break
+    qmodel.calc_qparams()
 
-    criterion = nn.CrossEntropyLoss().cuda(args.gpu)
-    optimizer = torch.optim.SGD(
-        model.parameters(),
-        args.lr,
-        momentum=args.momentum,
-        weight_decay=args.weight_decay,
-    )
-    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    scheduler = StepLR(optimizer, step_size=30, gamma=0.1)
-    if args.resume:
-        if os.path.isfile(args.resume):
-            print("=> loading checkpoint '{}'".format(args.resume))
-            if args.gpu is None:
-                checkpoint = torch.load(args.resume)
-            else:
-                # Map model to be loaded to specified single gpu.
-                loc = "cuda:{}".format(args.gpu)
-                checkpoint = torch.load(args.resume, map_location=loc)
-            args.start_epoch = checkpoint["epoch"]
-            best_acc1 = checkpoint["best_acc1"]
-            if args.gpu is not None:
-                # best_acc1 may be from a checkpoint from a different GPU
-                best_acc1 = best_acc1.to(args.gpu)
-            model.load_state_dict(checkpoint["state_dict"])
-            optimizer.load_state_dict(checkpoint["optimizer"])
-            scheduler.load_state_dict(checkpoint["scheduler"])
-            print(
-                "=> loaded checkpoint '{}' (epoch {})".format(
-                    args.resume, checkpoint["epoch"]
-                )
-            )
-        else:
-            print("=> no checkpoint found at '{}'".format(args.resume))
+    criterion = nn.CrossEntropyLoss()
+    qmodel.set_quant(w_quant=True, a_quant=True)
+    acc1 = validate(val_loader, qmodel, criterion, args)
 
-    cudnn.benchmark = True
-
-    best_acc1 = 0
-    for epoch in range(args.start_epoch, args.epochs):
-        if args.multiprocessing_distributed:
-            train_sampler.set_epoch(epoch)
-        # train for one epoch
-        train(train_loader, model, criterion, optimizer, epoch, args)
-        scheduler.step()
-
-        # evaluate on validation set
-        if not args.multiprocessing_distributed or (
-            args.multiprocessing_distributed and dist.get_rank() == 0
-        ):
-            acc1 = validate(val_loader, model, criterion, args)
-            # remember best acc@1 and save checkpoint
-            is_best = acc1 > best_acc1
-            best_acc1 = max(acc1, best_acc1)
-
-            save_checkpoint(
-                {
-                    "epoch": epoch + 1,
-                    "arch": args.arch,
-                    "state_dict": model.state_dict(),
-                    "best_acc1": best_acc1,
-                    "optimizer": optimizer.state_dict(),
-                    "scheduler": scheduler.state_dict(),
-                },
-                is_best,
-            )
-    print("Training is Done, best: {}".format(best_acc1))
-
-
-def train(train_loader, model, criterion, optimizer, epoch, args):
-    batch_time = AverageMeter("Time", ":6.3f")
-    data_time = AverageMeter("Data", ":6.3f")
-    losses = AverageMeter("Loss", ":.4e")
-    top1 = AverageMeter("Acc@1", ":6.2f")
-    top5 = AverageMeter("Acc@5", ":6.2f")
-    progress = ProgressMeter(
-        len(train_loader),
-        [batch_time, data_time, losses, top1, top5],
-        prefix="Epoch: [{}]".format(epoch),
-    )
-
-    # switch to train mode
-    model.train()
-
-    end = time.time()
-    for i, (images, target) in enumerate(train_loader):
-        # measure data loading time
-        data_time.update(time.time() - end)
-
-        if torch.cuda.is_available():
-            images = images.cuda(args.gpu, non_blocking=True)
-            target = target.cuda(args.gpu, non_blocking=True)
-
-        # compute output
-        output = model(images)
-        loss = criterion(output, target)
-
-        # measure accuracy and record loss
-        acc1, acc5 = accuracy(output, target, topk=(1, 5))
-        losses.update(loss.item(), images.size(0))
-        top1.update(acc1[0], images.size(0))
-        top5.update(acc5[0], images.size(0))
-
-        # compute gradient and do SGD step
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        # measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
-
-        if i % args.print_freq == 0:
-            progress.display(i)
+    qmodel.export_onnx(torch.randn(1, 3, 224, 224), name="q{}.onnx".format(args.arch))
 
 
 def validate(val_loader, model, criterion, args):
@@ -493,22 +352,6 @@ def accuracy(output, target, topk=(1,)):
             correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
-
-
-def setup_for_distributed(is_master):
-    """
-    This function disables printing when not in master process
-    """
-    import builtins as __builtin__
-
-    builtin_print = __builtin__.print
-
-    def print(*args, **kwargs):
-        force = kwargs.pop("force", False)
-        if is_master or force:
-            builtin_print(*args, **kwargs)
-
-    __builtin__.print = print
 
 
 if __name__ == "__main__":
