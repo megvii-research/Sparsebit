d6ea7990f263d8d46dc95ad91a5e7624e1e9412f
sunpeiqin <speiqin@gmail.com>
2022-11-21 12:49:21 +0800

    del setup_for_distributed

--- examples/quantization_aware_training/imagenet1k/basecase/main.py
+++ examples/quantization_aware_training/imagenet1k/basecase/main.py
@@ -484,21 +484,5 @@ def accuracy(output, target, topk=(1,)):
         return res
 
 
-def setup_for_distributed(is_master):
-    """
-    This function disables printing when not in master process
-    """
-    import builtins as __builtin__
-
-    builtin_print = __builtin__.print
-
-    def print(*args, **kwargs):
-        force = kwargs.pop("force", False)
-        if is_master or force:
-            builtin_print(*args, **kwargs)
-
-    __builtin__.print = print
-
-
 if __name__ == "__main__":
     main()
