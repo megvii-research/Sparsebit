be33c59b1b4d3e748a6705b431481625a867e48e
huangyechang <huangyechang@megvii.com>
2023-04-13 10:25:01 +0800

    add svd

--- large_language_models/llama/quantization/convert.py
+++ large_language_models/llama/quantization/convert.py
@@ -61,7 +61,9 @@ def get_wikitext2(nsamples, seed, seqlen, model_name):
 
 
 @torch.no_grad()
-def llama_sequential(model, dataloader, dev, means=None, stds=None, extra_bit_allocation={}):
+def llama_sequential(
+    model, dataloader, dev, means=None, stds=None, extra_bit_allocation={}
+):
     print("Starting ...")
 
     use_cache = model.config.use_cache
@@ -366,11 +368,14 @@ if __name__ == "__main__":
     extra_bit_allocation = {}
     if args.mixbit_config is not None:
         import json
+
         extra_bit_allocation = json.load(open(args.mixbit_config, "r"))
     print(extra_bit_allocation)
 
     # convert
-    quantizers = llama_sequential(model, dataloader, DEV, extra_bit_allocation=extra_bit_allocation)
+    quantizers = llama_sequential(
+        model, dataloader, DEV, extra_bit_allocation=extra_bit_allocation
+    )
 
     # evaluation
     print("The Perplexity on wikiText2: ")
