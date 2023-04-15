932a4068cdd4a2d9d202aae5cfa54805f69ffbf6
sunpeiqin <speiqin@gmail.com>
2023-04-15 17:22:58 +0800

    lint

--- large_language_models/alpaca-qlora/convert_pack32topack8.py
+++ large_language_models/alpaca-qlora/convert_pack32topack8.py
@@ -12,12 +12,19 @@ def main(args):
             print(k)
             assert v.dtype == torch.int, "weight in checkpoint must be int32!"
             v_list = []
-            for i in range(v.shape[0]): # for oc
-                v0_8b = (v[i] >> 0*8) & 0xff
-                v1_8b = (v[i] >> 1*8) & 0xff
-                v2_8b = (v[i] >> 2*8) & 0xff
-                v3_8b = (v[i] >> 3*8) & 0xff
-                v_8b = torch.cat([v0_8b[None, ...], v1_8b[None, ...], v2_8b[None, ...], v3_8b[None, ...]])
+            for i in range(v.shape[0]):  # for oc
+                v0_8b = (v[i] >> 0 * 8) & 0xFF
+                v1_8b = (v[i] >> 1 * 8) & 0xFF
+                v2_8b = (v[i] >> 2 * 8) & 0xFF
+                v3_8b = (v[i] >> 3 * 8) & 0xFF
+                v_8b = torch.cat(
+                    [
+                        v0_8b[None, ...],
+                        v1_8b[None, ...],
+                        v2_8b[None, ...],
+                        v3_8b[None, ...],
+                    ]
+                )
                 v_list.append(v_8b)
             new_qweight[k] = torch.cat(v_list).to(torch.int8)
 
@@ -29,9 +36,10 @@ def main(args):
 
 if __name__ == "__main__":
     import argparse
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('checkpoint')
-    parser.add_argument('output')
+    parser.add_argument("checkpoint")
+    parser.add_argument("output")
     args = parser.parse_args()
 
     main(args)
