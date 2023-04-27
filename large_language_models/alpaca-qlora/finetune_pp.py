755a9de7880be96286a4167be2585213d0ecfc90
Jiang-Stan <jiangstan@outlook.com>
2023-04-27 13:43:47 +0800

    support pp for llama qlora

---
+++ large_language_models/alpaca-qlora/finetune_pp.py
@@ -0,0 +1,235 @@
+import os
+import argparse
+import torch
+from torch.utils.data import DataLoader, DistributedSampler, RandomSampler
+from datasets import load_dataset
+import transformers
+from transformers.optimization import AdamW
+from transformers import LlamaTokenizer, get_linear_schedule_with_warmup
+from transformers.trainer_utils import seed_worker, set_seed
+from transformers.trainer_pt_utils import IterableDatasetShard
+from qlora import get_peft_qmodel
+from utils import load_qllama
+from tqdm import tqdm
+from peft import (
+    prepare_model_for_int8_training,
+    LoraConfig,
+)
+
+
+def main(args):
+
+    BATCH_SIZE = args.batch_size
+    GRADIENT_ACCUMULATION_STEPS = BATCH_SIZE // args.micro_batch_size
+    EPOCHS = 3  # we don't need 3 tbh
+    LEARNING_RATE = 3e-4  # the Karpathy constant
+    CUTOFF_LEN = 256  # 256 accounts for about 96% of the data
+    LORA_R = 8
+    LORA_ALPHA = 16
+    LORA_DROPOUT = 0.05
+    TRAIN_SET_SIZE = None
+    VAL_SET_SIZE = 2000
+
+    # Need to initialize RPC framework first.
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "29500"
+    torch.distributed.rpc.init_rpc("worker", rank=0, world_size=1)
+
+    config = transformers.AutoConfig.from_pretrained(args.model)
+
+    pp_kwargs = {
+        "chunks": args.chunks,
+        "checkpoint": args.pp_checkpoint,
+    }
+
+    model = load_qllama(config, args.cachedir, pp_kwargs)
+    model = prepare_model_for_int8_training(model)
+    model.seq_len = 2048
+    peft_func = get_peft_qmodel
+    tokenizer = LlamaTokenizer.from_pretrained(args.model, add_eos_token=True)
+    config = LoraConfig(
+        r=LORA_R,
+        lora_alpha=LORA_ALPHA,
+        target_modules=["q_proj", "v_proj"],
+        lora_dropout=LORA_DROPOUT,
+        bias="none",
+        task_type="QUANT_CAUSAL_LM",
+    )
+    model = peft_func(model, config)
+    tokenizer.pad_token_id = 0  # unk. we want this to be different from the eos token
+    data = load_dataset("json", data_files="alpaca_data.json")
+
+    train_val = data["train"].train_test_split(
+        train_size=TRAIN_SET_SIZE, test_size=VAL_SET_SIZE, shuffle=False, seed=42
+    )
+    train_data = train_val["train"]
+    val_data = train_val["test"]
+
+    def generate_prompt(data_point):
+        # sorry about the formatting disaster gotta move fast
+        if data_point["input"]:
+            return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{data_point["instruction"]}
+
+### Input:
+{data_point["input"]}
+
+### Response:
+{data_point["output"]}"""
+        else:
+            return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+### Instruction:
+{data_point["instruction"]}
+
+### Response:
+{data_point["output"]}"""
+
+    def tokenize(prompt):
+        # there's probably a way to do this with the tokenizer settings
+        # but again, gotta move fast
+        result = tokenizer(
+            prompt,
+            truncation=True,
+            max_length=CUTOFF_LEN + 1,
+            padding="max_length",
+        )
+        return {
+            "input_ids": result["input_ids"][:-1],
+            "attention_mask": result["attention_mask"][:-1],
+        }
+
+    train_data = train_data.map(lambda x: tokenize(generate_prompt(x)))
+    val_data = val_data.map(lambda x: tokenize(generate_prompt(x)))
+
+    data_collator = transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False)
+
+    signature_columns = ["input_ids", "attention_mask"]
+    ignored_columns = list(set(train_data.column_names) - set(signature_columns))
+
+    train_dataset = train_data.remove_columns(ignored_columns)
+
+    generator = torch.Generator()
+    generator.manual_seed(42)
+    set_seed(42)
+
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=args.micro_batch_size,
+        sampler=RandomSampler(train_dataset, generator=generator),
+        collate_fn=data_collator,
+        drop_last=False,
+        num_workers=0,
+        pin_memory=True,
+        worker_init_fn=seed_worker,
+    )
+
+    if isinstance(train_dataloader, DataLoader) and isinstance(
+        train_dataloader.sampler, DistributedSampler
+    ):
+        train_dataloader.sampler.set_epoch(0)
+    elif hasattr(train_dataloader, "dataset") and isinstance(
+        train_dataloader.dataset, IterableDatasetShard
+    ):
+        train_dataloader.dataset.set_epoch(0)
+
+    epoch_iterator = train_dataloader
+
+    model.print_trainable_parameters()
+
+    optimizer = AdamW(
+        filter(lambda p: p.requires_grad, model.parameters()),
+        lr=LEARNING_RATE,
+        weight_decay=0.0,
+        eps=1e-8,
+    )
+    lr_scheduler = get_linear_schedule_with_warmup(
+        optimizer=optimizer,
+        num_warmup_steps=100,
+        num_training_steps=len(epoch_iterator) // GRADIENT_ACCUMULATION_STEPS,
+    )
+
+    scaler = torch.cuda.amp.GradScaler()
+    loss_fct = torch.nn.CrossEntropyLoss()
+
+    # IMPORTANT! model.eval() -> model.train() enable requant 4-bit weights
+    model.eval()
+    model.train()
+
+    step = 0
+    accumulated_loss = 0
+    for inputs in tqdm(epoch_iterator):
+        step += 1
+        with torch.cuda.amp.autocast(cache_enabled=True, dtype=torch.float16):
+            labels = inputs.pop("labels")
+            outputs = model(**inputs)
+            outputs["logits"] = outputs["logits"].float()
+            labels = labels.to(outputs["logits"].device)
+            shift_logits = outputs["logits"][..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            # Flatten the tokens
+            shift_logits = shift_logits.view(-1, shift_logits.shape[-1])
+            shift_labels = shift_labels.view(-1)
+            # Enable model parallelism
+            shift_labels = shift_labels.to(shift_logits.device)
+            loss = loss_fct(shift_logits, shift_labels)
+
+        loss /= GRADIENT_ACCUMULATION_STEPS
+        accumulated_loss += loss.item()
+        scaler.scale(loss).backward()
+
+        if step % GRADIENT_ACCUMULATION_STEPS == 0:
+            scaler.unscale_(optimizer)
+            torch.nn.utils.clip_grad_norm_(
+                filter(lambda p: p.requires_grad, model.parameters()), max_norm=1.0
+            )
+            scaler.step(optimizer)
+            scaler.update()
+            lr_scheduler.step()
+            model.zero_grad()
+            tqdm.write(
+                "{"
+                + "'loss': {0:1.4f}, 'learning_rate': {1:2.6f}, 'epoch': {2:3.2f}".format(
+                    accumulated_loss,
+                    optimizer.param_groups[0]["lr"],
+                    step / len(epoch_iterator),
+                )
+                + "}"
+            )
+            accumulated_loss = 0
+
+    print("Peak memory usage for GPUs: ", end="")
+    for i in range(len(model.model.devices)):
+        print(
+            "cuda:{}: {}, ".format(
+                i, sizeof_fmt(torch.cuda.memory_stats(i)["allocated_bytes.all.peak"])
+            ),
+            end="",
+        )
+    print()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model", type=str, help="model name")
+    parser.add_argument("cachedir", type=str, help="path to 4bit checkpoint")
+    parser.add_argument(
+        "--batch_size", type=int, default=128, help="Batch size for training."
+    )
+    parser.add_argument(
+        "--micro_batch_size",
+        type=int,
+        default=64,
+        help="Micro batch size for gradient accumulating.",
+    )
+    parser.add_argument(
+        "--chunks", type=int, default=4, help="Number of trunks for pp."
+    )
+    parser.add_argument(
+        "--pp_checkpoint", type=str, default="except_last", help="checkpoint for pp`."
+    )
+    args = parser.parse_args()
+    main(args)
---
+++ large_language_models/alpaca-qlora/model_pp.py
@@ -0,0 +1,250 @@
+import math
+import torch
+import torch.nn as nn
+from torch.distributed.pipeline.sync import Pipe
+import torch.distributed.pipeline.sync.utils as pipe_utils
+from typing import Optional
+from transformers import LlamaConfig
+from transformers.models.llama.modeling_llama import (
+    LlamaDecoderLayer,
+    _make_causal_mask,
+    _expand_mask,
+)
+
+
+def get_first_device(model):
+    if model.devices:
+        return model.devices[0]
+    else:
+        return torch.cuda.current_device()
+
+
+class LlamaEmbedLayer(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.embed_tokens = nn.Embedding(
+            config.vocab_size, config.hidden_size, config.pad_token_id
+        )
+
+    def _prepare_decoder_attention_mask(
+        self, attention_mask, input_shape, inputs_embeds, past_key_values_length
+    ):
+        # create causal mask
+        # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
+        combined_attention_mask = None
+        if input_shape[-1] > 1:
+            combined_attention_mask = _make_causal_mask(
+                input_shape,
+                inputs_embeds.dtype,
+                device=inputs_embeds.device,
+                past_key_values_length=past_key_values_length,
+            )
+
+        if attention_mask is not None:
+            # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
+            expanded_attn_mask = _expand_mask(
+                attention_mask, inputs_embeds.dtype, tgt_len=input_shape[-1]
+            ).to(inputs_embeds.device)
+            combined_attention_mask = (
+                expanded_attn_mask
+                if combined_attention_mask is None
+                else expanded_attn_mask + combined_attention_mask
+            )
+
+        return combined_attention_mask
+
+    def forward(self, input_ids, attention_mask):
+        batch_size, seq_length = input_ids.shape
+        past_key_values_length = 0
+        # build position_ids
+        position_ids = torch.arange(
+            past_key_values_length,
+            seq_length + past_key_values_length,
+            dtype=torch.long,
+            device=input_ids.device,
+        )
+        position_ids = position_ids.unsqueeze(0).view(-1, seq_length)
+
+        inputs_embeds = self.embed_tokens(input_ids)
+
+        attention_mask = self._prepare_decoder_attention_mask(
+            attention_mask,
+            (batch_size, seq_length),
+            inputs_embeds,
+            past_key_values_length,
+        )
+
+        return inputs_embeds, attention_mask, position_ids
+
+
+class LlamaDecoderLayerWrapped(LlamaDecoderLayer):
+    def __init__(self, config: LlamaConfig):
+        super().__init__(config)
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask,
+        position_ids,
+    ):
+        """
+        Args:
+            hidden_states (`torch.FloatTensor`): input to the layer of shape `(batch, seq_len, embed_dim)`
+            attention_mask (`torch.FloatTensor`, *optional*): attention mask of size
+                `(batch, 1, tgt_len, src_len)` where padding elements are indicated by very large negative values.
+            output_attentions (`bool`, *optional*):
+                Whether or not to return the attentions tensors of all attention layers. See `attentions` under
+                returned tensors for more detail.
+            use_cache (`bool`, *optional*):
+                If set to `True`, `past_key_values` key value states are returned and can be used to speed up decoding
+                (see `past_key_values`).
+            past_key_value (`Tuple(torch.FloatTensor)`, *optional*): cached past key and value projection states
+        """
+
+        residual = hidden_states
+        hidden_states = self.input_layernorm(hidden_states)
+
+        # Self Attention
+        hidden_states, self_attn_weights, present_key_value = self.self_attn(
+            hidden_states=hidden_states,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+        )
+        hidden_states = residual + hidden_states
+
+        # Fully Connected
+        residual = hidden_states
+        hidden_states = self.post_attention_layernorm(hidden_states)
+        hidden_states = self.mlp(hidden_states)
+        hidden_states = residual + hidden_states
+
+        return hidden_states, attention_mask, position_ids
+
+
+class LlamaRMSNorm(nn.Module):
+    def __init__(self, hidden_size, eps=1e-6):
+        """
+        LlamaRMSNorm is equivalent to T5LayerNorm
+        """
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.variance_epsilon = eps
+
+    def forward(self, hidden_states, attention_mask, position_ids):
+        variance = hidden_states.to(torch.float32).pow(2).mean(-1, keepdim=True)
+        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
+
+        # convert into half-precision if necessary
+        if self.weight.dtype in [torch.float16, torch.bfloat16]:
+            hidden_states = hidden_states.to(self.weight.dtype)
+
+        return self.weight * hidden_states
+
+
+class LlamaSequential(nn.Sequential):
+    def __init__(self, config: LlamaConfig):
+        super().__init__()
+        # self.padding_idx = config.pad_token_id
+        # self.vocab_size = config.vocab_size
+        # embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, self.padding_idx)
+        embed_layer = LlamaEmbedLayer(config)
+        d_layers = nn.ModuleList(
+            [LlamaDecoderLayerWrapped(config) for _ in range(config.num_hidden_layers)]
+        )
+        norm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+        # self.gradient_checkpointing = False
+        # Initialize weights and apply final processing
+        # self.post_init()
+
+        layers = [embed_layer]
+        layers.extend([layer for layer in d_layers])
+        layers.extend([norm, lm_head])
+
+        super().__init__(*layers)
+
+
+class LlamaForCausalLMWrappedForPipe(nn.Module):
+    def __init__(self, config: LlamaConfig):
+        super().__init__()
+        self.config = config
+        self.model = LlamaSequential(self.config)
+
+    def pipe_sequential_model(self, chunks=1, pp_checkpoint="never"):
+        assert chunks >= 1 and isinstance(
+            chunks, int
+        ), "chunks must be a interger greater than 1"
+        num_devices = torch.cuda.device_count()
+        num_layers = len(self.model)
+        balance = []
+        layers_assigned = 0
+        for i in range(num_devices):
+            x = (num_layers - layers_assigned) / (num_devices - i)
+            if x.is_integer():
+                balance.append(int(x))
+                layers_assigned += x
+            else:
+                balance.append(math.ceil(x))
+                layers_assigned += math.ceil(x)
+
+        model = pipe_utils.partition_model(self.model, balance)
+        model_p = Pipe(model, chunks=chunks, checkpoint=pp_checkpoint)
+        del model
+        self.model = model_p
+
+    def prepare_inputs_for_generation(
+        self,
+        input_ids,
+        past_key_values=None,
+        attention_mask=None,
+        inputs_embeds=None,
+        **kwargs,
+    ):
+        if past_key_values:
+            input_ids = input_ids[:, -1:]
+
+        position_ids = kwargs.get("position_ids", None)
+        if attention_mask is not None and position_ids is None:
+            # create position_ids on the fly for batch generation
+            position_ids = attention_mask.long().cumsum(-1) - 1
+            position_ids.masked_fill_(attention_mask == 0, 1)
+            if past_key_values:
+                position_ids = position_ids[:, -1].unsqueeze(-1)
+
+        # if `inputs_embeds` are passed, we only want to use them in the 1st generation step
+        if inputs_embeds is not None and past_key_values is None:
+            model_inputs = {"inputs_embeds": inputs_embeds}
+        else:
+            model_inputs = {"input_ids": input_ids}
+
+        model_inputs.update(
+            {
+                "position_ids": position_ids,
+                "past_key_values": past_key_values,
+                "use_cache": kwargs.get("use_cache"),
+                "attention_mask": attention_mask,
+            }
+        )
+        return model_inputs
+
+    def forward(
+        self,
+        input_ids: torch.LongTensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        *args,
+        **kwargs,
+    ):
+        try:
+            first_device = get_first_device(self.model)
+            input_ids = input_ids.to(first_device)
+            attention_mask = attention_mask.to(first_device)
+            outputs = self.model(input_ids, attention_mask).local_value()
+        except Exception as e:
+            raise RuntimeError(
+                f"training failed on {torch.distributed.get_rank()}"
+            ) from e
+        hidden_states = outputs
+        # hidden_states = self.norm(hidden_states)
+        # logits = self.lm_head(hidden_states)
+        return {"logits": hidden_states}
